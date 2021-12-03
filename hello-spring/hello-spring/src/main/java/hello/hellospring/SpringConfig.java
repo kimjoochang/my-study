d1aebb804d07fa77b481224358fef9041edff1a6
kimjoochang <kimjc0707@naver.com>
2021-12-03 10:15:39 +0900

    Learned Up to JPA & AOP

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,29 +1,44 @@
 package hello.hellospring;
 
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
-  private DataSource dataSource;
+  private final MemberRepository memberRepository;
 
-  public SpringConfig(DataSource dataSource) {
-    this.dataSource = dataSource;
+  //@PersistenceContext
+  //  private EntityManager em;
+
+  //  private DataSource dataSource;
+
+  //  public SpringConfig(DataSource dataSource, EntityManager em) {
+  //    this.dataSource = dataSource;
+  //    this.em = em;
+  //  }
+
+  public SpringConfig(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
   }
 
   @Bean
   public MemberService memberService() {
-    return new MemberService(memberRepository());
+    return new MemberService(memberRepository);
   }
 
-  @Bean
-  public MemberRepository memberRepository() {
-    //    return new MemoryMemberRepository();
-    return new JdbcMemberRepository(dataSource);
-  }
+  //  @Bean
+  //  public TimeTraceAop TimeTraceAop() {
+  //    return new TimeTraceAop();
+  //  }
+
+  //  @Bean
+  //  public MemberRepository memberRepository() {
+  //    return new MemoryMemberRepository();
+  //return new JdbcMemberRepository(dataSource);
+  //return new JdbcTemplateMemberRepository(dataSource);
+  // return new JpaMemberRepository(em);
+  //}
 }
--- hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,16 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id; // 시스템에서 임의로 정해주는 아이디
+
   private String name;
 
   public Long getId() {
